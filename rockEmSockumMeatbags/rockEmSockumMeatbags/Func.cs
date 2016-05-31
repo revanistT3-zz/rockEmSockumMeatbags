a64fca1702c4193272733c1937b41b61592d6ee8
revanistT3 <dylanpaulmcrae@gmail.com>
2016-05-31 08:35:44 -0400

    added Func

---
+++ rockEmSockumMeatbags/rockEmSockumMeatbags/Func.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rockEmSockumMeatbags
+{
+    class Func
+    {
+        public static void safeDraw(SpriteBatch spriteBatch, Action f)
+        {
+            spriteBatch.Begin();
+            f();
+            spriteBatch.End();
+        }
+    }
+}
